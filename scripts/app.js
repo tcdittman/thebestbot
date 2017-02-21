6f77b8bebdddafd86e69c8bb53f8a4e26d9c354e
Tyler Dittman <tcdittman@gmail.com>
2017-02-20 20:52:49 -0800

    random scrit stuff

--- scripts/app.js
+++ scripts/app.js
@@ -2,4 +2,25 @@ module.exports = function(robot) {
   robot.hear(/fizz/, function(res) {
      return res.send("buzz");
    });
+
+   robot.respond(/you are my best (.*)/i, function(res) {
+     var relation;
+     relation = res.match[1];
+     if (relation === "friend") {
+       return res.emote(happy);
+     } else if (relation === "enemy") {
+       return res.emote(sad);
+     } else {
+       return res.emote(blank);
+     }
+   });
+
+
+   var lulz;
+   lulz = ['lol', 'rofl', 'lmao'];
+
+   return robot.respond(/lulz/i, function(res) {
+     return res.send(res.random(lulz));
+   });
+
 };
